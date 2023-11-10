fc56de34b385ef30111bb85d268036ba3d7c0552
comus3 <come.plantin.carrenard@gmail.com>
2023-11-10 09:12:22 +0100

    final working version

--- script.js
+++ script.js
@@ -36,6 +36,27 @@ class Animal {
   
     return ageMoyen;
   }
+
+  function updateAverageAge() {
+    ul.textContent = ""; // Efface la liste actuelle
+    //ul.innerHtml = ""; 
+    averageAgeDiv.textContent = "";
+  
+    const types = ["Chien", "Chat"];
+    types.forEach(type => {
+      const ageMoyen = calculerAgeMoyenParType(type, animalList);
+      const p = document.createElement("p");
+      p.textContent = `Âge moyen des ${type}s : ${ageMoyen} ans`;
+      averageAgeDiv.appendChild(p);
+    });
+  
+    // Récrée la liste d'animaux
+    animalList.getAllAnimals().forEach(animal => {
+      const li = document.createElement("li");
+      li.textContent = animal;
+      ul.appendChild(li);
+    });
+  }
   
   const animalForm = document.getElementById("animalForm");
   const nameInput = document.getElementById("name");
@@ -46,39 +67,22 @@ class Animal {
   // Créez l'objet AnimalList
 const animalList = new AnimalList();
 
-// Ajoutez quelques animaux initiaux
+// Ajoutez quelques animaux initiaux pour testing
 const chien1 = new Animal("Chien", "Max", 3);
 const chien2 = new Animal("Chien", "MMelk  ", 4);
 const chat1 = new Animal("Chat", "Toutou", 5);
 const chat2 = new Animal("Chat", "Kitty, Hello", 7);
 
-
+//ajout dans la liste
 animalList.addAnimal(chien1);
 animalList.addAnimal(chien2);
 animalList.addAnimal(chat1);
 animalList.addAnimal(chat2);
 
 const ul = document.getElementById("animalList");
-const averageAgeDiv = document.getElementById("averageAge");
+var averageAgeDiv = document.getElementById("averageAge");
 
-function updateAverageAge() {
-  ul.innerHTML = ""; // Efface la liste actuelle
 
-  const types = ["Chien", "Chat"];
-  types.forEach(type => {
-    const ageMoyen = calculerAgeMoyenParType(type, animalList);
-    const p = document.createElement("p");
-    p.textContent = `Âge moyen des ${type}s : ${ageMoyen} ans`;
-    averageAgeDiv.appendChild(p);
-  });
-
-  // Récrée la liste d'animaux
-  animalList.getAllAnimals().forEach(animal => {
-    const li = document.createElement("li");
-    li.textContent = animal;
-    ul.appendChild(li);
-  });
-}
 
 updateAverageAge();
 
@@ -95,8 +99,4 @@ animalForm.addEventListener("submit", function (e) {
   ageInput.value = "";
 
   updateAverageAge();
-});
-
-typeInput.addEventListener("input", function () {
-  typeValueOutput.textContent = typeInput.value === "1" ? "Chien" : "Chat";
 });
